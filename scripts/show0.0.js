99f18cb91ab6d715fb0c8118259a78782b1435e0
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2014-03-28 22:50:22 +0530

    Starting developement of show

--- scripts/show0.0.js
+++ scripts/show0.0.js
@@ -4,10 +4,17 @@
 
 Show = new (function(){
     
-    this.init = function(){
+    this.init = function(parent){
         /*
          * Initialize show
+         * 
+         * parent: parent DOM element for module
+         * 
          */
+        
+        parent = $(parent);
+        $('<div class="sidebar" id="sidebar"></div>').appendTo(parent);
+        $('<div class="slides" id="slides"></div>').appendTo(parent);
     }
     
     this.getMenu = function(){
