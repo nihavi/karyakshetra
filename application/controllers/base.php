fb857c08f0a4f1b0be0215b28cbbbc645b2952a0
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2014-04-05 00:04:34 +0530

    module Submit initiated

--- application/controllers/base.php
+++ application/controllers/base.php
@@ -11,12 +11,14 @@ class Base extends CI_Controller
             'base' => 'base0.0.js',
             'dash' => 'dash0.0.js',
             'akruti' => 'akruti0.0.js',
-            'show' => 'show0.0.js'
+            'show' => 'show0.0.js',
+            'submit' => 'submit0.0.js'
         );
         $module_style_repo = array(
             'dash' => 'dash0.0.css',
             'akruti' => 'akruti0.0.css',
-            'show' => 'show0.0.css'
+            'show' => 'show0.0.css',
+            'submit' => 'submit0.0.css'
         );
         if( !array_key_exists($module, $module_scripts_repo) )
         {
