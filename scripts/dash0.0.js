acdc7c36c086b4896d836a36a501334c2c3b2f86
Vivek Ghaisas <v.a.ghaisas@gmail.com>
2014-04-05 17:34:00 +0530

    Base: Added skeleton dash0.0.js
    Now, {root}/dash, or even just {root}, loads a dummy application which
    is just basically a copy of Base with some menus to test and demo
    interface functionality.

---
+++ scripts/dash0.0.js
@@ -0,0 +1,16 @@
+Dash = new(function(){
+    this.init = function(){
+    
+    };
+
+    this.getMenu = function(){
+        return {}
+    };
+    
+    this.resize = function(){
+    
+    };
+
+})();
+
+module = Dash;
