d94b85c825b3063ad55db61b5b5a3ee4449fdde7
tanmaysahay94 <tanmaysahay94@gmail.com>
2014-04-06 10:41:49 +0530

    Submit: Started adding helper functions for base class
    Going to have breakfast.

--- scripts/submit0.0.js
+++ scripts/submit0.0.js
@@ -8,13 +8,30 @@ Submit = new(function(){
         //meta information
         //form id, form title to be obtained from the back-end
         this.children = [];
-        this.children.push(new Page());  
+        this.children.push(new Page());
     };
     
+    Form.prototype = {
+
+        addPages = function(pages){
+            this.children.push(pages);
+            return this;
+        }
+
+        addPagesAtStart = function(pages){
+            this.children.unshift(pages);
+            return this;
+        }
+
+        movePageToEnd = function(page){
+        }
+
+    }
+
     var Page = function(){
 
         this.containers = [];
-        this.containters.push(new Control());
+        this.containers.push(new Control());
     };
 
     var Control = function(){
