e0a86a18236892c21c0a83667a43682ac44edbef
p0ff <abhishek.k@students.iiit.ac.in>
2014-04-10 13:28:21 +0530

    ....

--- scripts/aksharam0.0.js
+++ scripts/aksharam0.0.js
@@ -3,12 +3,25 @@ var Aksharam = new(function(){
 	function MyDoc() {
         this.characterCount = 0;
         this.docBody = document.createElement("div");
-        this.docBody["classname"] = "mydoc";
+        this.docBody["className"] = "mydoc";
         this.docBody.contentEditable = "true";
+        this.docWidth = 80;
+        this.padding = [1, 2, 3];
         this.render();
     }
     
     var docProto = MyDoc.prototype;
+    
+    docProto.render = function() {
+		var docStyle = this.docBody.style;
+		docStyle.width = this.docWidth + "vw";
+		docStyle.height = "100%";
+		docStyle.paddingTop = this.padding[0];
+		docStyle.paddingLeft = this.padding[1];
+		docStyle.paddingRight = this.padding[2];
+		docStyle.height = this.initialHeight + "vw";
+		document.getElementById("editable").appendChild(this.docBody);
+	};
     
     this.init = function(){
 		this.newDoc = new MyDoc();		
