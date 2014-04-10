7bed1106b4dec0aa1936f2729d1daf05b7630ff4
Vivek Ghaisas <v.a.ghaisas@gmail.com>
2014-04-10 16:44:30 +0530

    Dev: Refactored some code to match coding conventions

--- application/controllers/setupdonotuse.php
+++ application/controllers/setupdonotuse.php
@@ -9,11 +9,39 @@
 class Setupdonotuse extends CI_Controller {
     function index()
     {
-        $this->load->model('setupdb');
+        $this->load->model('Setup_model', 'setup');
         echo "<pre>";
-        $this->setupdb->setupTables();
+        $this->setup->setupTables();
         echo "</pre>";
         echo "Database is ready if you cannot see any error in log!";
     }
 }
-?>
+
+/*
+    function savefile()
+    {
+        $this->db->trans_start();
+        
+        //saving data in files
+        $data = array(
+            'fname'=>'file_name',
+            'path'=>'file_path',
+            'ftype'=>'bit_pattern',
+            'pid'=>'parent_id',
+        );
+        $this->db->set('modified', 'NOW()', FALSE);
+        $this->db->insert('files',$data);
+        $file_id = $this->db->insert_id();
+    
+        //saving data in filepermissions
+        $data = array(
+            'fid'=> $file_id,
+            'pid'=> 'parent_id',
+            'rights'=>'rights_given'
+        );
+        $this->db->insert('filepermissions',$data);
+        
+        
+        $this->db->trans_complete(); 
+    }
+*/
