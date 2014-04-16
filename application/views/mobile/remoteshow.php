7b4db78264b0b407f8e8a67b936d2e170967199e
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2014-04-16 22:01:03 +0530

    Misc: Remote for show working

--- application/views/mobile/remoteshow.php
+++ application/views/mobile/remoteshow.php
@@ -6,6 +6,34 @@
         <title>Karyakshetra | Doordarshan</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script>
+            var currFileId = <?php echo $fid; ?>;
+            var baseUrl = '<?php echo base_url(); ?>';
+    
+            var sendOp = function(op){
+                xmlhttp=new XMLHttpRequest();
+                xmlhttp.open("POST",baseUrl+'opqueue/addop',true);
+                xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+                xmlhttp.send('fid='+currFileId+'&op='+op);
+                
+                /*$.ajax({
+                    type: 'POST',
+                    url: baseUrl+'opqueue/addop',
+                    data: {
+                        fid: currFileId,
+                        op: op
+                    },
+                    success: function(data){
+                        //Do nothing
+                        //TODO: Failiure detection
+                    }
+                });*/
+            }
+            
+            var next = function(){
+                sendOp('ne');
+            }
+        </script>
     </head>
     <body>
         <!--[if lt IE 8]>
