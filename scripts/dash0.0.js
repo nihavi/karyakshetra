dc20bd619559854cc0455c612b27b28279aa67da
Vivek Ghaisas <v.a.ghaisas@gmail.com>
2014-04-17 13:00:48 +0530

    Base: Dashboard file listing and opening interface working

--- scripts/dash0.0.js
+++ scripts/dash0.0.js
@@ -7,42 +7,46 @@ Dash = new(function(){
             dataType: "json",
             url: response.baseUrl + 'storage/dash/',
             success: function(data) {
+                
                 var files = data.files;
+                
                 for (var i=0;i<files.length;++i) {
+                
                     var file = files[i];
-                    var a = $('<div></div>');
-                    var b = $('<a></a>')
-                    b.attr('target', '_blank');
-                    b.attr('href', file.module + '/' + file.id);
-                    a.addClass('file');
-                    b.text(file.name);
-                    a.append(b);
-                    a.prepend($('<input class="file-selector" type="checkbox">')
+                
+                    var f = $('<div></div>');
+                    f.addClass('file');
+                    
+                    var a = $('<a></a>')
+                    a.attr('target', '_blank');
+                    a.attr('href', file.module + '/' + file.id);
+                    a.text(file.name);
+            
+                    f.append($('<input class="file-selector" type="checkbox">')
                         .on('change', function() {
                             var input = $(this);
-                            /*
-                            if(input.is(':checked')) {
-                                input.closest('.file').addClass('focus');
-                            }
-                            else {
-                                input.closest('.file').removeClass('focus');
-                            }
-                            */
                         }
                     ));
-                    a.on('click', function() {
-                        var file = $(this);
-                        if (file.hasClass('focus')) {
-                            file.removeClass('focus');
-                            file.find('input.file-selector').prop('checked', false);
-                        }
-                        else {
-                            file.addClass('focus');
-                            file.find('input.file-selector').prop('checked', true);
+            
+                    f.append(a);
+                    
+                    f.on('click', function(e) {
+                        
+                        if (!$(e.target).is('a')) {
+                            var file = $(this);
+                            if (file.hasClass('focus')) {
+                                file.removeClass('focus');
+                                file.find('input.file-selector').prop('checked', false);
+                            }
+                            else {
+                                file.addClass('focus');
+                                file.find('input.file-selector').prop('checked', true);
+                            }
                         }
                     });
                     
-                    $('.file-list').append(a);
+                    $('.file-list').append(f);
+                    
                 }
                 
             }
