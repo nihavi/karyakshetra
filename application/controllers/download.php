665d4ee24c435324e554a7ff4537ff1660b9a270
Vivek Ghaisas <v.a.ghaisas@gmail.com>
2014-04-26 22:43:58 +0530

    Base: Download file(s) from dashboard implemented

--- application/controllers/download.php
+++ application/controllers/download.php
@@ -4,7 +4,7 @@ class Download extends CI_Controller {
     
     function file($fid = '')
     {
-        if ($fid != '')
+        if ($fid !== '')
         {
             $this->load->model('File_model', 'file');
             
@@ -27,6 +27,60 @@ class Download extends CI_Controller {
                     force_download($file->fname . '.' . $module['name'], $this->zip->get_zip());
                 }
             }
+        }
+    }
+    
+    function files($flist = '')
+    {
+        if ($flist !== '')
+        {
+            $flist = explode("-", $flist);
+            
+            $this->load->model('File_model', 'file');
+            $this->load->helper('module_helper');
+            $this->load->library('zip');
+            
+            $files_list = array();
+            
+            for ($i = 0; $i < count($flist); ++$i)
+            {
+                if (! is_numeric($flist[$i]))
+                {
+                    continue;
+                }
+                
+                $fid = $flist[$i];
+                
+                $file = $this->file->get_file($fid);
+                
+                $fpath = $file->path;
+                
+                $this->load->helper('module_helper');
+                
+                $module = module($file->ftype);
+                
+                if ($module)
+                {
+                    $this->zip->clear_data();
+                    $this->zip->read_dir($fpath, false, $fpath);
+                    
+                    $files[] = array(
+                        'name' => $file->fname . '.' . $module['name'],
+                        'data' => $this->zip->get_zip()
+                    );
+                }
+            }
+            
+            $this->zip->clear_data();
+            
+            for ($i = 0; $i < count($files); ++$i)
+            {
+                $file = $files[$i];
+                $this->zip->add_data($file['name'], $file['data']);
+            }
+            
+            $this->load->helper('download');
+            force_download('Karyakshetra files ' . date('Y-m-d') . '.zip' , $this->zip->get_zip());
         }
     }
     
